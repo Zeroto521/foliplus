6e2f90f0adee375695e1e6f140a8f1aa5329b4cf
40% <Zeroto521@gmail.com>
2026-07-23 02:11:34 +0800

    feat: refine heatmap icon polygon coordinates

--- foliplus/js/HeatmapControl.js
+++ foliplus/js/HeatmapControl.js
@@ -64,14 +64,14 @@
   const SVGs = {
     HEXAGON: `
       <svg viewBox="0 0 24 24">
-        <polygon points="12,1 20,5.5 20,14.5 12,19 4,14.5 4,5.5" stroke-width="1.5"/>
-        <polygon points="9.5,8.5 12,7 14.5,8.5 14.5,10.5 12,12 9.5,10.5" stroke-width="1"/>
-        <polygon points="9.5,3.5 12,2 14.5,3.5 14.5,5.5 12,7 9.5,5.5" stroke-width="1"/>
-        <polygon points="13.5,5.5 16.5,4 19.5,5.5 19.5,7.5 16.5,9 13.5,7.5" stroke-width="1"/>
-        <polygon points="13.5,12.5 16.5,11 19.5,12.5 19.5,14.5 16.5,16 13.5,14.5" stroke-width="1"/>
-        <polygon points="9.5,14.5 12,13 14.5,14.5 14.5,16.5 12,18 9.5,16.5" stroke-width="1"/>
-        <polygon points="4.5,12.5 7.5,11 10.5,12.5 10.5,14.5 7.5,16 4.5,14.5" stroke-width="1"/>
-        <polygon points="4.5,5.5 7.5,4 10.5,5.5 10.5,7.5 7.5,9 4.5,7.5" stroke-width="1"/>
+        <polygon points="12,3 20.5,7.5 20.5,16.5 12,21 3.5,16.5 3.5,7.5" stroke-width="1.5"/>
+        <polygon points="9.5,10.5 12,9 14.5,10.5 14.5,12.5 12,14 9.5,12.5" stroke-width="1"/>
+        <polygon points="9.5,5.5 12,4 14.5,5.5 14.5,7.5 12,9 9.5,7.5" stroke-width="1"/>
+        <polygon points="14,7.5 17,6 20.5,7.5 20.5,9.5 17,11 14,9.5" stroke-width="1"/>
+        <polygon points="14,14.5 17,13 20.5,14.5 20.5,16.5 17,18 14,16.5" stroke-width="1"/>
+        <polygon points="9.5,16.5 12,15 14.5,16.5 14.5,18.5 12,20 9.5,18.5" stroke-width="1"/>
+        <polygon points="3.5,14.5 7,13 10,14.5 10,16.5 7,18 3.5,16.5" stroke-width="1"/>
+        <polygon points="3.5,7.5 7,6 10,7.5 10,9.5 7,11 3.5,9.5" stroke-width="1"/>
       </svg>`,
   };
 
