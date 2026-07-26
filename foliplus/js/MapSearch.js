5f734daaa8e22ef67d790ea99bd44756208827ca
40% <Zeroto521@gmail.com>
2026-07-26 17:49:22 +0800

    remove extra spaces

--- foliplus/js/MapSearch.js
+++ foliplus/js/MapSearch.js
@@ -60,10 +60,10 @@
   // ==================== SVG Icons ====================
   const SVGs = {
     SEARCH: `
-        <svg viewBox="0 0 24 24">
-          <circle cx="10.5" cy="10.5" r="6.5"/>
-          <line x1="15.5" y1="15.5" x2="21" y2="21"/>
-        </svg>`,
+      <svg viewBox="0 0 24 24">
+        <circle cx="10.5" cy="10.5" r="6.5"/>
+        <line x1="15.5" y1="15.5" x2="21" y2="21"/>
+      </svg>`,
   };
 
   foliplus.registerHintIcon(CONST.name, SVGs.SEARCH);
