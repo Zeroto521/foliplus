82115e07dbe7d5e9096027e680382d14f6a7bc58
40% <Zeroto521@gmail.com>
2026-08-08 08:04:34 +0800

    refactor: remove IIFE wrappers from FullscreenControl and ScaleControl modules

--- foliplus/js/FullscreenControl/FullscreenControl.js
+++ foliplus/js/FullscreenControl/FullscreenControl.js
@@ -3,59 +3,84 @@ import { CLASSES, containerId } from "./FullscreenControl.const.js";
 import * as ICONS from "./FullscreenControl.icon.js";
 import { bindFullscreenEvents, toggleFullscreen } from "./FullscreenControl.logic.js";
 
-(function () {
-  const CONF = window.foliplus.CONFIG.FullscreenControl;
-  if (!requireRuntime(CONF.name)) return;
+const CONF = window.foliplus.CONFIG.FullscreenControl;
+requireRuntime(CONF.name);
 
-  const foliplus = window.foliplus;
-  const _ = (k) => (foliplus.gt ? foliplus.gt(k) : k);
-  foliplus.registerHintIcon(CONF.name, ICONS.MAXIMIZE);
+const foliplus = window.foliplus;
+const _ = (k) => (foliplus.gt ? foliplus.gt(k) : k);
+foliplus.registerHintIcon(CONF.name, ICONS.MAXIMIZE);
 
-  const cid = containerId(CONF.name, CONF.position);
+const cid = containerId(CONF.name, CONF.position);
 
-  class FullscreenControl extends L.Control {
-    onAdd() {
-      if (map.zoomControl) map.removeControl(map.zoomControl);
-      else {
-        const zoomEl = map.getContainer().querySelector(".leaflet-control-zoom");
-        if (zoomEl) zoomEl.remove();
-      }
-
-      const outer = foliplus.dom.el("div", { class: "leaflet-bar leaflet-control", id: cid });
-      const container = foliplus.dom.el("div", {
-        class: `${CLASSES.FULLSCREEN_BAR} foliplus-ctrl-fold`,
-        parent: outer,
-      });
-
-      foliplus.dom.el("button", {
-        class: `${CLASSES.TOOL_BTN} ${CLASSES.ZOOM_IN}`,
-        "aria-label": _(`${CONF.name}.zoom_in`), title: _(`${CONF.name}.zoom_in`),
-        parent: container,
-        onclick: (e) => { L.DomEvent.stopPropagation(e); map.zoomIn(); },
-      }, { html: ICONS.ZOOM_IN });
-
-      foliplus.dom.el("button", {
-        class: `${CLASSES.TOOL_BTN} ${CLASSES.ZOOM_OUT}`,
-        "aria-label": _(`${CONF.name}.zoom_out`), title: _(`${CONF.name}.zoom_out`),
-        parent: container,
-        onclick: (e) => { L.DomEvent.stopPropagation(e); map.zoomOut(); },
-      }, { html: ICONS.ZOOM_OUT });
-
-      const fsBtn = foliplus.dom.el("button", {
-        class: `${CLASSES.TOOL_BTN} ${CLASSES.FS_TOGGLE}`,
-        "aria-label": _(`${CONF.name}.title`), title: _(`${CONF.name}.title`),
-        parent: container,
-        onclick: (e) => { L.DomEvent.stopPropagation(e); toggleFullscreen(map, fsBtn, container); },
-      }, { html: ICONS.MAXIMIZE });
-
-      L.DomEvent.disableClickPropagation(outer);
-      L.DomEvent.disableScrollPropagation(outer);
-
-      bindFullscreenEvents(map, fsBtn, container);
-
-      return outer;
+class FullscreenControl extends L.Control {
+  onAdd() {
+    if (map.zoomControl) map.removeControl(map.zoomControl);
+    else {
+      const zoomEl = map.getContainer().querySelector(".leaflet-control-zoom");
+      if (zoomEl) zoomEl.remove();
     }
-  }
 
-  new FullscreenControl({ position: CONF.position }).addTo(map);
-})();
+    const outer = foliplus.dom.el("div", {
+      class: "leaflet-bar leaflet-control",
+      id: cid,
+    });
+    const container = foliplus.dom.el("div", {
+      class: `${CLASSES.FULLSCREEN_BAR} foliplus-ctrl-fold`,
+      parent: outer,
+    });
+
+    foliplus.dom.el(
+      "button",
+      {
+        class: `${CLASSES.TOOL_BTN} ${CLASSES.ZOOM_IN}`,
+        "aria-label": _(`${CONF.name}.zoom_in`),
+        title: _(`${CONF.name}.zoom_in`),
+        parent: container,
+        onclick: (e) => {
+          L.DomEvent.stopPropagation(e);
+          map.zoomIn();
+        },
+      },
+      { html: ICONS.ZOOM_IN },
+    );
+
+    foliplus.dom.el(
+      "button",
+      {
+        class: `${CLASSES.TOOL_BTN} ${CLASSES.ZOOM_OUT}`,
+        "aria-label": _(`${CONF.name}.zoom_out`),
+        title: _(`${CONF.name}.zoom_out`),
+        parent: container,
+        onclick: (e) => {
+          L.DomEvent.stopPropagation(e);
+          map.zoomOut();
+        },
+      },
+      { html: ICONS.ZOOM_OUT },
+    );
+
+    const fsBtn = foliplus.dom.el(
+      "button",
+      {
+        class: `${CLASSES.TOOL_BTN} ${CLASSES.FS_TOGGLE}`,
+        "aria-label": _(`${CONF.name}.title`),
+        title: _(`${CONF.name}.title`),
+        parent: container,
+        onclick: (e) => {
+          L.DomEvent.stopPropagation(e);
+          toggleFullscreen(map, fsBtn, container);
+        },
+      },
+      { html: ICONS.MAXIMIZE },
+    );
+
+    L.DomEvent.disableClickPropagation(outer);
+    L.DomEvent.disableScrollPropagation(outer);
+
+    bindFullscreenEvents(map, fsBtn, container);
+
+    return outer;
+  }
+}
+
+new FullscreenControl({ position: CONF.position }).addTo(map);
--- foliplus/js/ScaleControl/ScaleControl.js
+++ foliplus/js/ScaleControl/ScaleControl.js
@@ -1,50 +1,48 @@
 import { requireRuntime } from "../runtime/runtime.guard.js";
 
-(function () {
-  const CONF = window.foliplus.CONFIG.ScaleControl;
-  const CLASSES = {
-    SCALE_WRAP: "foliplus-scale-wrap",
-    SCALE_ZOOM_LABEL: "foliplus-scale-zoom-label",
-  };
+const CONF = window.foliplus.CONFIG.ScaleControl;
+const CLASSES = {
+  SCALE_WRAP: "foliplus-scale-wrap",
+  SCALE_ZOOM_LABEL: "foliplus-scale-zoom-label",
+};
 
-  // ==================== Runtime Guard ====================
-  if (!requireRuntime(CONF.name)) return;
+// ==================== Runtime Guard ====================
+requireRuntime(CONF.name);
 
-  // ==================== Dependencies ====================
-  const foliplus = window.foliplus;
-  const _ = (k) => (foliplus.gt ? foliplus.gt(k) : k);
+// ==================== Dependencies ====================
+const foliplus = window.foliplus;
+const _ = (k) => (foliplus.gt ? foliplus.gt(k) : k);
 
-  // ==================== Control Definition ====================
-  class ScaleControl extends L.Control {
-    onAdd() {
-      const scaleCtrl = L.control.scale({
-        metric: CONF.isMetric,
-        imperial: !CONF.isMetric,
+// ==================== Control Definition ====================
+class ScaleControl extends L.Control {
+  onAdd() {
+    const scaleCtrl = L.control.scale({
+      metric: CONF.isMetric,
+      imperial: !CONF.isMetric,
+    });
+    scaleCtrl._map = this._map;
+    const wrap = scaleCtrl.onAdd(this._map);
+    wrap.classList.add(CLASSES.SCALE_WRAP);
+
+    // ==================== Zoom Label ====================
+    if (CONF.show_zoom) {
+      const zoomLabel = foliplus.dom.el("span", {
+        class: CLASSES.SCALE_ZOOM_LABEL,
+        parent: wrap,
       });
-      scaleCtrl._map = this._map;
-      const wrap = scaleCtrl.onAdd(this._map);
-      wrap.classList.add(CLASSES.SCALE_WRAP);
-
-      // ==================== Zoom Label ====================
-      if (CONF.show_zoom) {
-        const zoomLabel = foliplus.dom.el("span", {
-          class: CLASSES.SCALE_ZOOM_LABEL,
-          parent: wrap,
-        });
-        const updateZoom = () => {
-          zoomLabel.textContent = _(`${CONF.name}.zoom_label`).replace(
-            "{zoom}",
-            this._map.getZoom(),
-          );
-        };
-        updateZoom();
-        this._map.on("zoomend", updateZoom);
-        this._map.on("unload", () => this._map.off("zoomend", updateZoom));
-      }
-
-      return wrap;
+      const updateZoom = () => {
+        zoomLabel.textContent = _(`${CONF.name}.zoom_label`).replace(
+          "{zoom}",
+          this._map.getZoom(),
+        );
+      };
+      updateZoom();
+      this._map.on("zoomend", updateZoom);
+      this._map.on("unload", () => this._map.off("zoomend", updateZoom));
     }
-  }
 
-  new ScaleControl({ position: CONF.position }).addTo(map);
-})();
+    return wrap;
+  }
+}
+
+new ScaleControl({ position: CONF.position }).addTo(map);
