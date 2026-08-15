0533b9ba5d11e0488025fbe24a68fb6c9d555f05
40% <Zeroto521@gmail.com>
2026-08-16 01:29:19 +0800

    fix: remove duplicate import of ensureModes in MeasureControl

--- foliplus/js/MeasureControl/manager.ts
+++ foliplus/js/MeasureControl/manager.ts
@@ -5,7 +5,6 @@ import { ensureModes } from "#core/mode.js";
 import { adjustPanelZIndex } from "#common/panel.js";
 import * as Storage from "#common/storage.js";
 import { HINT_DURATION } from "#core/hint.js";
-import { ensureModes } from "#core/mode/index.js";
 import * as CONST from "./const.js";
 import * as SVGs from "./icon.js";
 import {
